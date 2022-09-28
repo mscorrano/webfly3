42afb6c6e943338a4cb4bee38e3f2004802decd9
marco_scorrano <marco.scorrano@dsolutions.it>
2022-09-28 22:58:14 +0200

    Update config.php

--- conf/config.php
+++ conf/config.php
@@ -4,16 +4,16 @@
 	
 	// DATABASE ACCESS...
 	global $_FRWK;
-	$_FRWK["PARAMS"]["DATABASE"]["DB_SERVER"] 	= "localhost";
-	$_FRWK["PARAMS"]["DATABASE"]["USERNAME"]  	= "portale";
-	$_FRWK["PARAMS"]["DATABASE"]["PASSWD"]    	= "R7Wo8eheS7JE";
-	$_FRWK["PARAMS"]["DATABASE"]["DATABASE"]   	= "webfly";
+	$_FRWK["PARAMS"]["DATABASE"]["DB_SERVER"] 	= "XXXXXXXXXXX";
+	$_FRWK["PARAMS"]["DATABASE"]["USERNAME"]  	= "XXXXXXXXXXX";
+	$_FRWK["PARAMS"]["DATABASE"]["PASSWD"]    	= "XXXXXXXXXXX";
+	$_FRWK["PARAMS"]["DATABASE"]["DATABASE"]   	= "XXXXXXXXXXX";
 	
 	// LDAP
 	$_FRWK["LDAP"]					= true;
-	$_FRWK["LDAP_SERVER"]			= "ldap://10.16.18.200";
+	$_FRWK["LDAP_SERVER"]			= "ldap://XXX.XXX.XXX.XXX";
 	$_FRWK["LDAP_PORT"]				= "389";
-	$_FRWK["LDAP_DOMAIN"]   		= "D_PESCARA";
+	$_FRWK["LDAP_DOMAIN"]   		= "XXXXXXXXXXX";
 	
 	
 ?>
